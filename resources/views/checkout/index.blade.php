6e0574848119ce1af7e31d58554fcc8fb7da67df
BeingDeveloper96 <beingdeveloper96@gmail.com>
2020-01-21 10:14:46 +0500

    PhoneGap Geolocation

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -31,7 +31,7 @@
                         <div class="col-md-6">
                             <label>Postal/Zip Code</label>
                             <div class="form-wrap">
-                                {{ Form::number('billing_postalcode', null, ['placeholder' => 'Postal Code (Enter your postal code to generate the editable address)']) }}
+                                {{ Form::number('billing_postalcode', null, ['placeholder' => 'Postal Code (Enter your postal code to generate the editable address)','id' => 'billing_postal']) }}
                                 @error('billing_postalcode')
                                 <label for="billing_postalcode" class="text-danger">{{ $message }}</label>
                                 @enderror
@@ -41,7 +41,7 @@
                         <div class="col-md-6">
                             <label>Country</label>
                             <div class="form-wrap">
-                                {{ Form::select('country', $countries, null, ['placeholder' => 'Select Country']) }}
+                                {{ Form::select('country', $countries, null, ['placeholder' => 'Select Country', 'id' => 'billing_country']) }}
                                 @error('country')
                                 <label for="country" class="text-danger">{{ $message }}</label>
                                 @enderror
@@ -53,7 +53,7 @@
                             <label>Province </label>
                             <div class="form-wrap">
                                 <div class="form-wrap">
-                                    {{ Form::select('billing_province', $provinces, null, ['placeholder' => 'Select Province']) }}
+                                    {{ Form::select('billing_province', $provinces, null, ['placeholder' => 'Select Province', 'id' => 'billing_province']) }}
                                     @error('billing_province')
                                     <label for="billing_province" class="text-danger">{{ $message }}</label>
                                     @enderror
@@ -64,7 +64,7 @@
                             <label>City </label>
                             <div class="form-wrap">
                                 <div class="form-wrap">
-                                    {{ Form::select('billing_city', $cities, auth()->user()->city_id, ['placeholder' => 'Select City']) }}
+                                    {{ Form::select('billing_city', $cities, auth()->user()->city_id, ['placeholder' => 'Select City', 'id' => 'billing_city']) }}
                                     @error('billing_city')
                                     <label for="billing_city" class="text-danger">{{ $message }}</label>
                                     @enderror
@@ -76,7 +76,7 @@
                         <div class="col-md-12">
                             <label>Billing Address </label>
                             <div class="form-wrap">
-                                {{ Form::textarea('billing_address', auth()->user()->address, ['rows' => 3]) }}
+                                {{ Form::textarea('billing_address', auth()->user()->address, ['rows' => 3, 'id' => 'billing_address']) }}
                                 @error('billing_address')
                                     <label for="billing_address" class="text-danger">{{ $message }}</label>
                                 @enderror
@@ -87,7 +87,7 @@
                         <div class="col-md-12">
                             <label>Shipping Address </label>
                             <div class="form-wrap">
-                                {{ Form::textarea('shipping_address', auth()->user()->address, ['rows' => 3]) }}
+                                {{ Form::textarea('shipping_address', auth()->user()->address, ['rows' => 3, 'shipping_address']) }}
                                 @error('shipping_address')
                                     <label for="shipping_address" class="text-danger">{{ $message }}</label>
                                 @enderror
@@ -163,4 +163,56 @@
         {{ Form::close() }}
     </div>
 </div>
+@endsection
+
+@section('page_script')
+    <script>
+        let app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+            if(app) {  
+                /**
+                * Get Current Location
+                *
+                */
+                navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true });
+
+                /**
+                * Get Latitude and Longitude Cordinates and pass to getCity() to fetch the cityname
+                *
+                */
+                function onSuccess(position) {
+                    timestamp = position.timestamp;
+
+                    getCity(
+                        position.coords.latitude,
+                        position.coords.longitude
+                    );
+                }
+
+                /**
+                * Get cityname from Lat, Long and pass the cityname to getSalatDetails() to get the Salat Details
+                *
+                */
+                function getCity(latitude, longitude) {
+                    $.ajax({
+                        url: `https://geocode.xyz/${latitude},${longitude}?geoit=json`
+                    }).done(function(data) {
+                        let postal = data.postal ? data.postal : 000000;
+                        
+                        $('#billing_postal').val(postal)
+                        $('#billing_country option:contains('+data.country+')').attr("selected", "selected");
+                        $('#billing_province').val(data.region);
+                        $('#billing_city').val(data.city);
+                        $('#billing_address').val(data.staddress);
+                        $('#shipping_address').val(data.staddress);
+                        
+                    }).fail(function(data) {
+                        alert('Something Went Wrong While Fetching the city name..');
+                    });
+                }
+            
+                function onError(error) {
+                    console(error)
+                }
+            }
+    </script>
 @endsection
